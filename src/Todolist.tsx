9eddb60527181a9569fe74213aa53ef5bc13a930
Essam <esammohammd2006@gmail.com>
2025-02-21 11:04:19 +0300

    just removed a comment

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -86,18 +86,6 @@ function Todolist() {
           </div>
         ))}
       </div>
-
-      {/* <div className="m-3 flex flex-col ">
-        <div className="flex items-center space-x-2">
-          <Checkbox id="terms" />
-          <label
-            htmlFor="terms"
-            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            doing the dishes
-          </label>
-        </div>
-      </div> */}
     </div>
   );
 }
